b025ca16b2576f7eafd81b7de23f6dd1e8e74dc7
Hasan Alkarnosy <gruceing@gmail.com>
2022-08-28 18:11:16 +0300

    release

--- components/content/Features.vue
+++ components/content/Features.vue
@@ -1,6 +1,11 @@
 <template>
     <h1 my="10" un-text="white" font="bold">Powerful Features</h1>
     <div grid="~ cols-3 gap-6">
-        <Card v-for="item in 6" :key="item" />
+        <Card 
+            title="Bring you happiness"
+            icon="i-material-symbols-add-reaction-rounded"
+         v-for="item in 6" :key="item">
+            Bring you happiness wherever you are.
+         </Card>
     </div>
 </template>
