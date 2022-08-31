c556675b40781298caa405a098eb7090b7356940
Hassan Alkhalidi <5357529+Gruce@users.noreply.github.com>
2022-08-31 23:33:51 +0300

    mobile

--- components/Contents.vue
+++ components/Contents.vue
@@ -1,21 +1,19 @@
 <template>
-    <div v-if="notMobile" w="full lg:1/4">
+    <aside flex="~ col gap-2" w="1/4" py="4" class="invisible lg:visible" overflow="y-scroll">
         <ClientOnly>
-            <aside flex="~ col gap-2" py="4" overflow="y-scroll">
-                <span font="bold" un-text="xl white" mb="3">Table of Contents</span>
-                    <div flex="~ col gap-3">
-                        <div v-for="element in contents" :key="element.id">
-                            <a v-if="element.tag == 'h1'" :href="`#${element.id}`" decoration="none" font="bold" un-text="w-80 hover:primary" class="group my-4">
-                                {{ element.title }}
-                            </a>
-                            <a v-if="element.tag == 'h2'" :href="`#${element.id}`" decoration="none" un-text="sm w-50 hover:primary" class="group">
-                                <span un-text="w-10 group-hover:w-50">#</span> {{ element.title }}
-                            </a>
-                        </div>
-                    </div>
-            </aside>
+            <span font="bold" un-text="xl white" mb="3">Table of Contents</span>
+            <div flex="~ col gap-3">
+                <div v-for="element in contents" :key="element.id">
+                    <a v-if="element.tag == 'h1'" :href="`#${element.id}`" decoration="none" font="bold" un-text="w-80 hover:primary" class="group my-4">
+                        {{  element.title  }}
+                    </a>
+                    <a v-if="element.tag == 'h2'" :href="`#${element.id}`" decoration="none" un-text="sm w-50 hover:primary" class="group">
+                        <span un-text="w-10 group-hover:w-50">#</span> {{  element.title  }}
+                    </a>
+                </div>
+            </div>
         </ClientOnly>
-    </div>
+    </aside>
 </template>
 
 <script setup>
@@ -27,7 +25,7 @@ const contentQuery = ref(null)
 const contents = ref(null)
 
 const getContents = async (r) => {
-    if (r?.params?.slug?.length > 0){
+    if (r?.params?.slug?.length > 0) {
         const { data } = await useAsyncData('contents-' + r?.params?.slug?.join('-'), () => queryContent(...r.params.slug).findOne())
         contents.value = data.value?.excerpt?.children.filter(({ tag }) => tag == 'h2' || tag == 'h1').map(
             (element) => {
@@ -43,13 +41,13 @@ const getContents = async (r) => {
 
 getContents(route)
 
-watch (() => route, (r) => getContents(r), {deep: true})
+watch(() => route, (r) => getContents(r), { deep: true })
 
 
 const breakpoints = useBreakpoints(breakpointsTailwind)
 
 const notMobile = breakpoints.isGreater('lg')
-const isMobile = computed( () => !notMobile)
+const isMobile = computed(() => !notMobile)
 
 
 
