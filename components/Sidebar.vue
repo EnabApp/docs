c4f7eb254bb50f095340851cffbaf19cb7c22dd7
Hasan Alkarnosy <gruceing@gmail.com>
2022-09-01 00:57:33 +0300

    Update Sidebar.vue

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -1,7 +1,7 @@
 <template>
     <ClientOnly>
         <div h="full lg:auto" v-if="cond()" z="10" bg="black lg:transparent" position="absolute lg:relative" w="full lg:1/5">
-            <aside flex="~ col gap-3" w="full" py="4">
+            <aside flex="~ col gap-3" px="6 lg:0" py="6 lg:4">
                 <NavItem :item="nav" v-for="nav of navs" :key="nav._path" />
             </aside>
         </div>
