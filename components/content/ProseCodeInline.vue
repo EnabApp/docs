a850ca7f3463e4bad93714c791940ecebf29de52
Hassan Alkhalidi <5357529+Gruce@users.noreply.github.com>
2022-08-31 20:18:25 +0300

    qwd

--- components/content/ProseCodeInline.vue
+++ components/content/ProseCodeInline.vue
@@ -10,13 +10,13 @@ const props = defineProps(['color', 'label'])
 const styleColor = computed( () => {
     switch (props.color){
         case 'success':
-            return 'text-success'
+            return 'text-success-200'
         case 'error':
-            return 'text-error'
+            return 'text-error-400'
         case 'warning':
-            return 'text-warning'
+            return 'text-warning-200'
         case 'info':
-            return 'text-info'
+            return 'text-info-200'
         default:
             return 'text-white'
     }
