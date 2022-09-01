038cce5a435b350e83b2b064fda656f0ed75b00e
Hasan Alkarnosy <gruceing@gmail.com>
2022-09-01 09:31:10 +0300

    qw

--- components/Features.vue
+++ components/Features.vue
@@ -2,10 +2,39 @@
     <h1 p="6 lg:0" my="lg:10" un-text="white" font="bold">Powerful Features</h1>
     <div p="6 lg:0" grid="~ cols-1 lg:cols-3 gap-6">
         <Card 
-            title="Bring you happiness"
+            :title="item.title"
             icon="i-material-symbols-add-reaction-rounded"
-         v-for="item in 6" :key="item">
-            Bring you happiness wherever you are.
+         v-for="item in features" :key="item.title">
+            {{ item.description }}
          </Card>
     </div>
 </template>
+
+<script setup>
+    const features = ref([
+        {
+            title: "Easy to Start",
+            description: "Easy to start creating your own app with Enab. You can start with a simple template or with a custom one. You can also use the module-starter template to start a new module/service/app.",
+        },
+        {
+            title: "Easy to Use",
+            description: "Easy to use Enab with the best tools and technologies (Nuxt and VueJS) for the web. You can use the same codebase for both web and mobile apps with just a few ready to use tools for responsive.",
+        },
+        {
+            title: "Easy to Maintain",
+            description: "Easy to maintain your app with Enab. You can easily update your app with the latest version of Enab and you can easily push your app to production with just a command.",
+        },
+        {
+            title: "Wonderful Components",
+            description: "We have created a beautiful user interface for Enab with a lot of components that you can boost your development with.",
+        },
+        {
+            title: "Powerful Customization",
+            description: "You can easy custom your app with a highly customizable functions and flexiable Ui.",
+        },
+        {
+            title: "Powerful API",
+            description: "You can use our composables to easy add functionalities to your app. You can also use your own API to connect your app with your own backend.",
+        }
+    ])
+</script>
--- components/Intro.vue
+++ components/Intro.vue
@@ -1,10 +1,10 @@
 <template>
-    <div p="6 lg:0" m="lg:t-35 b-60" flex="~ col gap-4">
+    <div p="6 lg:0" m="lg:t-32 b-60" flex="~ col gap-4">
         <div flex="~ col basis-2/3" w="full lg:2/3">
-            <span font="bold" leading="5rem" un-text="2xl secondaryOp lg:left center">For a better YOU</span>
-            <span font="bold" leading="4rem lg:5rem" un-text="5xl lg:7xl white lg:left center">Making apps made easy for Enab Developers</span>
+            <p font="bold" leading="5rem" un-text="2xl w-30 lg:left center">For a better you.</p>
+            <span font="bold" leading="4rem lg:5rem" un-text="5xl lg:7xl white lg:left center" uppercase="~">Making apps now easy for Enab Developers</span>
             <p un-text="xl lg:2xl lg:left center w-60">
-                Nuxt Content reads the content/ directory in your project, parses .md, .yml, .csv and .json files to create a powerful data layer for your application. Use Vue components in Markdown with the MDC syntax.
+                Enab is a platform that helps you build apps easily with the best tools and technologies (Nuxt and VueJS) for the web. This documentation covers all the features of Enab and how to use them. You can also see the <a href="https://development.enab.app" un-text="white" decoration="none" target="_blank">official-development platform</a>.
             </p>
         </div>
         <div flex="basis-1/3" w="1/3">
