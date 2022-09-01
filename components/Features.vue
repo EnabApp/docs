03d447d7df98929faa9dd3befff7a5ccbde91ecc
Hasan Alkarnosy <gruceing@gmail.com>
2022-09-01 12:24:01 +0300

    qw

--- components/Features.vue
+++ components/Features.vue
@@ -3,7 +3,7 @@
     <div p="6 lg:0" grid="~ cols-1 lg:cols-3 gap-6">
         <Card 
             :title="item.title"
-            icon="i-material-symbols-add-reaction-rounded"
+            :icon="item.icon"
          v-for="item in features" :key="item.title">
             {{ item.description }}
          </Card>
@@ -13,28 +13,34 @@
 <script setup>
     const features = ref([
         {
-            title: "Easy to Start",
-            description: "Easy to start creating your own app with Enab. You can start with a simple template or with a custom one. You can also use the module-starter template to start a new module/service/app.",
+            title: "One Minute to Start",
+            description: "You can start with a simple template or with a custom one.",
+            icon: "i-material-symbols-timer-off-outline-rounded"
         },
         {
             title: "Easy to Use",
-            description: "Easy to use Enab with the best tools and technologies (Nuxt and VueJS) for the web. You can use the same codebase for both web and mobile apps with just a few ready to use tools for responsive.",
+            description: "Easy to use Enab with the best web technologies.",
+            icon: "i-icon-park-outline-easy"
         },
         {
-            title: "Easy to Maintain",
-            description: "Easy to maintain your app with Enab. You can easily update your app with the latest version of Enab and you can easily push your app to production with just a command.",
+            title: "Simple to Maintain",
+            description: "You can easily update your app with the latest version of Enab.",
+            icon: "i-material-symbols-rocket-launch-outline"
         },
         {
             title: "Wonderful Components",
-            description: "We have created a beautiful user interface for Enab with a lot of components that you can boost your development with.",
+            description: "We have created a beautiful UI with many components.",
+            icon: "i-bx-bxs-component"
         },
         {
             title: "Powerful Customization",
-            description: "You can easy custom your app with a highly customizable functions and flexiable Ui.",
+            description: "You can easy custom your app with a highly customizable functions.",
+            icon: "i-material-symbols-settings-input-component-outline"
         },
         {
             title: "Powerful API",
-            description: "You can use our composables to easy add functionalities to your app. You can also use your own API to connect your app with your own backend.",
+            description: "You can use your own API to connect your app with your own backend.",
+            icon: "i-file-icons-api-blueprint"
         }
     ])
 </script>
