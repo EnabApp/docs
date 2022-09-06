e6ced1cd5a9ac2aba2a8920a1227c0a6321c83bc
Hussein Najah <husseinnajah123@gmail.com>
2022-09-06 21:03:32 +0300

    Icons + Iplemintions

--- components/content/ProseCode.vue
+++ components/content/ProseCode.vue
@@ -12,12 +12,12 @@
                     {{ language }}
                 </span>
                 <span @click="previewToggle()" v-if="language == 'html' && !code.includes('<!---->')"  w="4" h="4" cursor="pointer" :class="[ previewState ? 'bg-w-20 hover:bg-opacity-30' : 'bg-w-5 hover:bg-opacity-10']" p="y-1 x-2"  border="~ w-5 hover:w-10 rounded" flex="~" justify="center" items="center">
-                    <div v-if="previewState" class="i-mdi-eye-off"></div>
-                    <div v-else class="i-ic-baseline-remove-red-eye"></div>
+                    <IconsEyeOff v-if="previewState" class="i-mdi-eye-off"></IconsEyeOff>
+                    <IconsEyeOff v-else class="i-ic-baseline-remove-red-eye"></IconsEyeOff>
                 </span>
                 <span @click="copy()" w="2" h="4" class="invisible group-hover:visible" cursor="pointer" bg="w-5 hover:opacity-10" p="y-1 x-2"  border="~ w-5 hover:w-10 rounded" flex="~" items="center">
-                    <div v-if="copied" class="i-fa6-solid-clipboard-check"></div>
-                    <div v-else class="i-fa6-solid-clipboard"></div>
+                    <IconsClipboardCheck v-if="copied" class="i-fa6-solid-clipboard-check"></IconsClipboardCheck>
+                    <IconsClipboard v-else class="i-fa6-solid-clipboard"></IconsClipboard>
                 </span>
             </div>
             <div un-text="md white" p="6" flex="~ col" items="start">
