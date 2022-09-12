397a15c6ec29480784b62c43e7a09b39d4ff97f0
Ahmed <mr.darkboss1@gmail.com>
2022-09-13 01:05:54 +0300

    Button

---
+++ components/content/Show/Show.vue
@@ -0,0 +1,5 @@
+<template>
+    <div p="x-3 y-8" border="secondaryOp dashed 2px rounded-xl" items="center">
+        <slot/>
+    </div>
+</template>
---
+++ components/content/Show/Zbutton.vue
@@ -0,0 +1,23 @@
+<template>
+    <div v-if="button =='color'" flex="~ gap-4" justify="center">
+         <UiButton color="primary" >primary</UiButton>
+         <UiButton color="info" >info</UiButton>
+         <UiButton color="success" >success</UiButton>
+         <UiButton color="warning" >warning</UiButton>
+         <UiButton color="error" >error</UiButton>
+    </div>
+    <div v-if="button =='outline'" flex="~ gap-4" justify="center">
+         <UiButton color="primary" :outline="true" >primary</UiButton>
+         <UiButton color="info" :outline="true" >info</UiButton>
+         <UiButton color="success" :outline="true" >success</UiButton>
+         <UiButton color="warning" :outline="true" >warning</UiButton>
+         <UiButton color="error" :outline="true" >error</UiButton>
+    </div>
+    <div v-if="button =='to'" flex="~" justify="center">
+         <UiButton color="info" to="https://enab.app/" >Enab App</UiButton>
+    </div>
+
+</template>
+<script setup>
+const props = defineProps(['button'])
+</script>
