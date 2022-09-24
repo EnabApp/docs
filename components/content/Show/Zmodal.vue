e768d787d787e50392627fb7f90685d371557f56
Ahmed <mr.darkboss1@gmail.com>
2022-09-24 13:45:28 +0300

    A

---
+++ components/content/Show/Zmodal.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <UiButton @click="stateModal = true">Show Modal</UiButton>
+  <Teleport to="body">
+    <UiModal v-model="stateModal" @cancel="modalCanceled">
+        <template #title> Title </template>
+      <div>body</div>
+    </UiModal>
+  </Teleport>
+  </div>
+</template>
+
+<script setup>
+const [stateModal, toggleModal] = useToggle(false);
+
+const modalCanceled = () => {
+  console.log("Canceled");
+  stateModal.value = false;
+};
+</script>
